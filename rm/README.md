bab6266ee16e57e4613a586b002bb7f1742d8db1
Mane Arzumanyan <manearzumanyan@Macintosh.local>
2021-10-26 00:07:58 +0400

    implemented rm-recursively functionality

--- rm/README.md
+++ rm/README.md
@@ -1,8 +1,9 @@
 # Readme about rm instrument
-rm file_path
+rm file_path -f
 
-rm program removes file if it is regular file 
+rm program removes specified file, and all files in it recursively
 - as the argument program gets the path of file
-- minimal argument count is 1, otherwise, the program will not run
-- program checks whether given path is for a regular file or not, if not prints error
+- minimal argument count is 2, otherwise, the program will not run (should always give a -f flag)
+- program checks whether given path is for a regular file or not 
+- if it is a directory, program removes content of the directory recursively
 - if path is for a regular file it removes file
--- rm/rm.c
+++ rm/rm.c
@@ -1,8 +1,10 @@
 //rm [OPTION]... FILE...
 #include <stdio.h>
-#include <unistd.h>
-#include <sys/stat.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <string.h>
+#include <unistd.h>
 
 int isRegular(char* path) {
    struct stat S;
@@ -10,17 +12,61 @@ int isRegular(char* path) {
    return S_ISREG(S.st_mode);
 }
 
+int isDir(char* path) {
+   struct stat S;
+   stat(path, &S);
+   return S_ISDIR(S.st_mode);
+}
+
+void removeFiles(char* path) {
+    char newPath[1000];
+    DIR* directory = opendir(path);
+    if(directory != NULL){
+        struct dirent* struct_;
+        while((struct_ = readdir(directory)) != NULL) {
+            if(strcmp(struct_->d_name , "..") == 0 || strcmp(struct_->d_name , ".") == 0 || strcmp(struct_->d_name , ".git") == 0) {
+              continue;
+            }
+            strcpy(newPath, path);
+            strcat(newPath, "/");
+            strcat(newPath, struct_->d_name);
+            if(isRegular(newPath)) {
+              printf("removed file -> %s\n", newPath);
+              remove(newPath);  
+            }
+            else if(isDir(newPath)) {
+              printf("enter next dir -> [%s]\n", newPath);
+              removeFiles(newPath);
+              rmdir(newPath);
+            }
+        }
+    }
+    closedir(directory);
+    rmdir(path);
+}
+
 int main(int argc, char **argv){
-  if (argc < 2){
+  if (argc < 3){
+    printf("need to specify all arguments");
     return -1;
   }
   char *p;
+  int run = 0;
   struct stat st;
-
+  for(int i = 0; i < argc; ++i){
+    if(strcmp( argv[i], "-f") == 0){
+      run = 1;
+    }
+  }
+  if(run == 0){
+    printf("-f flag not specified");
+    return -1;
+  }
   if (isRegular(argv[1])){
+    printf("removed file -> %s\n", argv[1]);
     remove(argv[1]);
-  }else{
-    printf("provide only files please\n");
+  } else{
+    removeFiles(argv[1]);
   }
   return 0;
 }
